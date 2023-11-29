90dbf2ed90153db15910203bd717fadef618794b
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-11-29 20:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //your JS code here. If required.
 // const output = document.getElementById("output");
 // const btn = document.getElementById("download-images-button");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
+let images = [
+	'https://picsum.photos/id/237/200/300',
+	'https://picsum.photos/id/238/200/300',
+	'https://picsum.photos/id/239/200/300'
+]; //urls of images
 
 document.getElementById('download-images-button').addEventListener('click',(event)=>{
 
